4fb58bb91bd4046c7cc844e146c3b99046ef58a3
nicholasjamessmith <njsmith6@gmail.com>
2023-09-26 17:53:44 -0400

    show page working

--- src/components/Review.js
+++ src/components/Review.js
@@ -5,12 +5,11 @@ function Review({ review }) {
   return <>
     <div>
       <Link to={`/review/${review.id}`}>
-        {/*<img src={review.image }></img>*/}
+        <img src={review.image}></img>
       </Link>
         <h4>{review.title_of_work}</h4>
         <h4>{review.artist}</h4>
         <h4>{review.review}</h4>
-      
     </div>
   </>
 }
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,16 @@
+import { Link, useLoaderData } from 'react-router-dom';
+
 function Show(props) {
-  return <h1>Show</h1>
+  const review = useLoaderData();
+  return <>
+    <div>
+      <img src={review.image}></img>
+      <h4>{review.title_of_work}</h4>
+      <h4>{review.artist}</h4>
+      <p>{review.review}</p>
+      <Link to="/">Back to Index</Link>
+    </div>
+  </>
 }
 
 export default Show
