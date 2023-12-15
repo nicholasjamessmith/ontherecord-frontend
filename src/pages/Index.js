0097dac37f086f0be92343bfaf580b066fd40801
nicholasjamessmith <njsmith6@gmail.com>
2023-12-15 15:49:57 -0500

    styled index form and submit button

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -6,13 +6,15 @@ function Index(props) {
   return <>
     <h1>On The Record__!</h1>
     <h2>Review Something...</h2>
+    {/*<div className="form">*/}
     <Form method="post" action="/create">
-      <input type="text" name="image" placeholder="Image link" />
-      <input type="text" name="title_of_work" placeholder="Title of work" />
-      <input type="text" name="artist" placeholder="Artist" />
-      <input type="text" name="review" placeholder="Review" />
-      <button>Submit Review__.</button>
+      <input className="input" type="text" name="image" placeholder="Image link" />
+      <input className="input" type="text" name="title_of_work" placeholder="Title of work" />
+      <input className="input" type="text" name="artist" placeholder="Artist" />
+      <input className="input" type="text" name="review" placeholder="Review" />
+      <button className="input" id="submitBtn">Submit Review</button>
     </Form>
+    {/*</div>*/}
     {reviews.map((review) => <Review key={review.id} review={review} />)}
   </>
 }
